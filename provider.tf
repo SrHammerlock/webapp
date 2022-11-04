0dd39def72c364b860edfb0f50cfa650067dcfb5
SrHammerlock <111486225+SrHammerlock@users.noreply.github.com>
2022-11-04 14:43:15 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,2 @@
 provider "aws" {
-  region = "us-west-2"
-  access_key = "ASIA42Y3YYYHVNFIKGJZ"
-  secret_key = "FLHNfDvpQ+NqvGSqY9sJouzeCRvmAjCr26B+g7+X"
-  token = "FwoGZXIvYXdzEPv//////////wEaDHm9O+sVpulOmCOSGCK7ARlqzCfI1Z6EsdRrry4WgH83MtgYXg5pt0rRbzlj7GIAogOo4wO2n6Plr5rkB2ghaYF1EE+G/GdIIWhUjmg6EbOv6dCSRvs4q6DauwXqHiF6ISevKAOJLptirXKcx1yYKo8wLklT41sWbp0JYZITXFN7ftc4hsHoeuGFnNT/a9fh1B0y5w+iEDeGYMG9dlMiu8wt8fbV7BomeLNkEOIkNHCIhZ92ppy0EhG3JpdxLRnLBY/k7Ib+MjtV0WoolbeTmwYyLfUEbkw2YlRyppsiiY5BdophVbRgLMzGFeXLGCEygdOVd08swPnNdujwbi7UTQ=="
 }
-
